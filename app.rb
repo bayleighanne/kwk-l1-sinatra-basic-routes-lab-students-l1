2b9e82b1e5f37458843e9cbce8c02c4972cda29d
bayleighanne <bayhaf718@gmail.com>
2018-06-12 22:40:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello World!"
+  end
+  get '/name' do 
+    "My name is Bayleigh"
+  end
+  get '/hometown' do 
+    "My home town is Awhatukee"
+  end
+  get '/favorite-song' do 
+    "My favorite song is 'Dear Mama"
+  end
 end
